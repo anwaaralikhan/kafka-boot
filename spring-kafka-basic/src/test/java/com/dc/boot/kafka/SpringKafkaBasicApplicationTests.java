a71053ba49f1f4aba6a650c0a89a5aedd576fadb
anwaaralikhan <manwaaronline@gmail.com>
2019-10-19 15:44:00 -0400

    Spring Kafka Project

---
+++ spring-kafka-basic/src/test/java/com/dc/boot/kafka/SpringKafkaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.boot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
